77658bf8677ef63250886e42266110091418512b
Shay Avraham <github@hinbit.com>
2025-03-07 11:35:01 +0200

    Create db_connection.php

---
+++ config/db_connection.php
@@ -0,0 +1,13 @@
+<?php
+$config = json_decode(file_get_contents(__DIR__ . '/db_connection.json'), true);
+
+try {
+    $pdo = new PDO(
+        "mysql:host={$config['host']};dbname={$config['dbname']};charset=utf8",
+        $config['username'],
+        $config['password']
+    );
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die(json_encode(["status" => "error", "message" => "Database connection failed: " . $e->getMessage()]));
+}
