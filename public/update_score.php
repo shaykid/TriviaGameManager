441cbdc36799992ce74d2918f8753905df7ed117
Shay Avraham <github@hinbit.com>
2025-03-09 14:49:49 +0200

    Delete public/update_score.php

--- public/update_score.php
+++
@@ -1,9 +0,0 @@
-<?php
-require '../config/db_connection.php';
-require '../scripts/score_manager.php';
-
-$user_id = $_GET['user_id'];
-$correct_answers = $_GET['correct_answers'];
-
-updateUserScore($pdo, $user_id, $correct_answers);
-?>
