512aeffacc80c5bb2640a27ebb7f391114b660c5
Shay Avraham <github@hinbit.com>
2025-03-09 15:01:59 +0200

    Create redirect_whatsapp.php

---
+++ public/redirect_whatsapp.php
@@ -0,0 +1,14 @@
+<?php
+// Get URL parameters or set defaults
+$wappnum = isset($_GET['WAPPNUM']) ? $_GET['WAPPNUM'] : '972544506093';
+$msgtext = isset($_GET['MSGTEXT']) ? $_GET['MSGTEXT'] : 'Lets Play';
+$id = isset($_GET['ID']) ? $_GET['ID'] : '1000001';
+
+// Prepare WhatsApp URL
+$encodedMessage = urlencode($msgtext . " " . $id);
+$whatsappURL = "https://wa.me/{$wappnum}?text={$encodedMessage}";
+
+// Redirect user to WhatsApp
+header("Location: $whatsappURL");
+exit;
+?>
