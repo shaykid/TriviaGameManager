87cde3a044096082eace4cdad832af4fedabec1a
Shay Avraham <github@hinbit.com>
2025-03-12 12:53:27 +0200

    Update redirect_whatsapp.php

--- public/redirect_whatsapp.php
+++ public/redirect_whatsapp.php
@@ -1,8 +1,14 @@
 <?php
+// Set content-type header to ensure UTF-8 encoding
+header('Content-Type: text/html; charset=utf-8');
+
 // Get parameters from URL, with defaults
 $wappnum = isset($_GET['WAPPNUM']) ? $_GET['WAPPNUM'] : '972527229106';
 $msgtext = isset($_GET['MSGTEXT']) ? $_GET['MSGTEXT'] : '🪴  אני רוצה לשחק במשחקי שיח 😂 ';
 $id = isset($_GET['ID']) ? $_GET['ID'] : '1000001';
+
+// Ensure the message is in UTF-8 encoding
+$msgtext = mb_convert_encoding($msgtext, 'UTF-8', 'auto');
 
 // Generate the new ID format as requested
 $currentMinute = date('i');  // minute at start
